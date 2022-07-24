9b64c961df156afcaedff93a19adf4a3854167eb
lulu <r.omoikane@gmail.com>
2022-07-24 15:56:53 +0900

    ログインボタン作成

--- src/html/index.php
+++ src/html/index.php
@@ -1,5 +1,6 @@
 <?php
-
+$site_title = 'koto-koto';
+$nav_title = 'koto-kotoへようこそ';
 
 ?>
 
@@ -13,7 +14,6 @@
     <!-- bootstrap cssの読み込み -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="../css/bootstrap/bootstrap.css">
-
     <!-- 自作cssの読み込み -->
     <link rel="stylesheet" href="../css/custom.css">
     <title>koto-kotoへようこそ</title>
@@ -21,14 +21,20 @@
 
 <body class="bg-light">
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">koto-koto</a>
+                <a class="navbar-brand" href="#">
+                    <h1><?= $site_title ?></h1>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <!-- 文字が見えないけど表示はされてる、ズレてるけど…なんで？ -->
+                            <p class="nav_title"><?= $nav_title ?></p>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 user_name
@@ -53,10 +59,17 @@
     </header>
 
     <main>
+        <div class="row">
+            <div class="col-12 col-md-8 col-xl-6">
+                <a href="#"><button type="button" class="btn btn-primary">ログイン</button></a>
+                <a href="#"><button type="button" class="btn btn-success">新規登録</button></a>
+            </div>
+        </div>
         <p>Hallo world</p>
         <p>linux環境を一旦削除、その後復元</p>
         <p>Githubとの連携が出来ているかチェック</p>
         <p>ubuntuにcloneした、プッシュのテスト</p>
+        <p>bootstrapのデフォルトの色を変更したい…</p>
     </main>
     <footer>
     </footer>
