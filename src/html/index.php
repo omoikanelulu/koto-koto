e825d050b5fc061ba4efd36410fd2f053d0c6705
omoikanelulu <r.omoikane@gmail.com>
2022-07-25 11:39:00 +0900

    bootstrapをカスタムする準備

--- src/html/index.php
+++ src/html/index.php
@@ -13,7 +13,7 @@ $nav_title = 'koto-kotoへようこそ';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap cssの読み込み -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="../css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap5.1.3/dist/css/bootstrap.css">
     <!-- 自作cssの読み込み -->
     <link rel="stylesheet" href="../css/custom.css">
     <title><?= $nav_title ?></title>
