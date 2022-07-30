e39e6f915777c33036d8c869ed520c71094d0c49
lulu <r.omoikane@gmail.com>
2022-07-30 12:52:40 +0900

    err_msg追加

--- src/class/config.php
+++ src/class/config.php
@@ -2,3 +2,4 @@
 $top_page_url = 'http://' . $_SERVER["HTTP_HOST"] . '/koto-koto/src/html/index.php';
 $site_title = 'koto-koto';
 $test_msg = '入力内容が表示される';
+$err_msg = '申し訳ございません<br>予期せぬエラーが発生いたしました<br>時間を置いてから再度お試しください';
