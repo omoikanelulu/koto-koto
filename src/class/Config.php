21c7d8194701cac10050bdba10ff219b4456542c
lulu <r.omoikane@gmail.com>
2022-07-30 13:55:41 +0900

    ファイル名を変更し再作成

---
+++ src/class/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+$top_page_url = 'http://' . $_SERVER["HTTP_HOST"] . '/koto-koto/src/html/index.php';
+$site_title = 'koto-koto';
+$test_msg = '入力内容が表示される';
+$session_info ='sessionに保存した内容';
+$err_msg = '申し訳ございません<br>予期せぬエラーが発生いたしました<br>時間を置いてから再度お試しください';
+
+
+// class Config
+// {
+//     const top_page_url = 'http://' . $_SERVER["HTTP_HOST"] . '/koto-koto/src/html/index.php';
+//     const site_title = 'koto-koto';
+//     const test_msg = '入力内容が表示される';
+//     const err_msg = '申し訳ございません<br>予期せぬエラーが発生いたしました<br>時間を置いてから再度お試しください';
+// }
