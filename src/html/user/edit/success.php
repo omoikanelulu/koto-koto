9706ded4da7400d4f459452cd935ab36d141d902
omoikanelulu <r.omoikane@gmail.com>
2022-08-01 11:01:40 +0900

    新規作成

---
+++ src/html/user/edit/success.php
@@ -0,0 +1,65 @@
+<?php
+require_once '../../../class/Config.php';
+$nav_title = 'ユーザ情報編集完了';
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap cssの読み込み -->
+    <link rel="stylesheet" href="../css/bootstrap5.1.3/dist/css/bootstrap.min.css">
+    <!-- 自作cssの読み込み -->
+    <link rel="stylesheet" href="../../../css/custom.css">
+    <title><?= $nav_title ?></title>
+</head>
+
+<body class="bg-light">
+    <header>
+        <nav class="navbar fixed-top zindex-fixed p-0 opacity-75 navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand row" href="<?= $top_page_url ?>">
+                    <h1><?= $site_title ?> |</h1>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item navbar-brand">
+                            <h4><?= $nav_title ?></h4>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="mt-5 container">
+            <div class="row row-cols-3 d-flex justify-content-center">
+                <div class="mb-4 col">
+                    <p>ユーザ情報編集が完了しました</p>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="mb-4 row row-cols-3 d-flex justify-content-center">
+                <div class="col">
+                    <a href=<?= $things_top_page_url ?>><button type="button" class="me-3 btn btn-primary">デキゴト記録へ</button></a>
+                </div>
+                <div class="col"></div>
+            </div>
+        </div>
+    </main>
+    <footer>
+    </footer>
+
+    <!-- bootstrap JavaScript Bundle with Popper -->
+    <script src="../css/bootstrap5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
