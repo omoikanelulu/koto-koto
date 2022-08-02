9e3f6de1e0ce9e62a730821637b17816c8f94d15
omoikanelulu <r.omoikane@gmail.com>
2022-08-02 11:14:02 +0900

    navbarのメニューをforeachで表示させる様に追加修正

--- src/html/things/index.php
+++ src/html/things/index.php
@@ -34,30 +34,30 @@ $nav_title = 'デキゴトを記録';
                         <li class="nav-item navbar-brand">
                             <h4><?= $nav_title ?></h4>
                         </li>
+                        <!-- ここからドロップダウンメニュー -->
+                        <!-- ページ移動メニュー -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 メニュー
                             </a>
                             <ul class="text-end dropdown-menu" aria-labelledby="navbarDropdown">
-                                <?php foreach ($menu_urls as $menu => $url) : ?>
+                                <?php foreach ($nav_menus as $menu => $url) : ?>
                                     <li><a class="dropdown-item" href="<?= $url ?>"><?= $menu ?></a></li>
                                 <?php endforeach ?>
                             </ul>
                         </li>
-
+                        <!-- ユーザメニュー -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 user_name
                             </a>
                             <ul class="text-end dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">ログアウト</a></li>
+                                <?php foreach ($nav_user_menus as $menu => $url) : ?>
+                                    <li><a class="dropdown-item" href=<?= $url ?>><?= $menu ?></a></li>
+                                <?php endforeach ?>
                             </ul>
                         </li>
+                        <!-- ここまでドロップダウンメニュー -->
                     </ul>
                 </div>
             </div>
