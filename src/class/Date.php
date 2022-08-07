12e10ddbcce5be4041b23074e035714debd88c40
lulu <r.omoikane@gmail.com>
2022-08-07 14:27:50 +0900

    $this_yearを求めるだけの存在

--- src/class/Date.php
+++ src/class/Date.php
@@ -1,51 +1,67 @@
 <?php
-// staticはやめる
+
 class Date
 {
-/**
- * 今年の年を指定する
- */
-public function__constract(){
-    $this_year = date('Y');
-}
+    /**
+     * 今年の年を代入する
+     */
+    public function __constract()
+    {
+        $this_year = date('Y');
+        return $this_year;
+    }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     /**
      * @param int $start 最小日
      * @param int $end 最大日
      */
-    public static function days_select($start, $end)
-    {
-        $days = array();
-        for ($i = $start; $i <= $end; $i++) {
-            $days[] = $i;
-        }
-        return $days;
-    }
+    // public function days_select($start, $end)
+    // {
+    //     $days = array();
+    //     for ($i = $start; $i <= $end; $i++) {
+    //         $days[] = $i;
+    //     }
+    //     return $days;
+    // }
 
     /**
      * @param int $start 最小月
      * @param int $end 最大月
      */
-    public static function months_select($start, $end)
-    {
-        $months = array();
-        for ($i = $start; $i <= $end; $i++) {
-            $months[] = $i;
-        }
-        return $months;
-    }
+    // public function months_select($start, $end)
+    // {
+    //     $months = array();
+    //     for ($i = $start; $i <= $end; $i++) {
+    //         $months[] = $i;
+    //     }
+    //     return $months;
+    // }
 
     /**
      * @param int $start 最小年
      * @param int $end 最大年
      */
-    public static function years_select($start, $end)
-    {
-        $years = array();
-        for ($i = $start; $i <= $end; $i++) {
-            $years[] = $i;
-        }
-        return $years;
-    }
+    // public function years_select($start, $end)
+    // {
+    //     $years = array();
+    //     for ($i = $start; $i <= $end; $i++) {
+    //         $years[] = $i;
+    //     }
+    //     return $years;
+    // }
 }
