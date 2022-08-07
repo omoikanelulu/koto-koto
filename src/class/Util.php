7ce89cd8651dcb05fcc2a5247193f795a60096ac
lulu <r.omoikane@gmail.com>
2022-08-07 14:36:37 +0900

    多分もう使用しない

--- src/class/Util.php
+++ src/class/Util.php
@@ -2,14 +2,15 @@
 
 class Util
 {
+    // Base.phpに移したのでもう使用しないと思われる
     /**
      * 現在のURLを取得する
      */
-    public static function get_uri()
-    {
-        $current_page = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        return $current_page;
-    }
+    // public static function get_uri()
+    // {
+    //     $current_page = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    //     return $current_page;
+    // }
 
     /**
      * 現在のファイル名を取得する
