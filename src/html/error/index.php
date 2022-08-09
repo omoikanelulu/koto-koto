f2741beb93b8de5d0769799259b4c5d194ce94b7
omoikanelulu <r.omoikane@gmail.com>
2022-08-09 14:37:17 +0900

    navbarが一段落

--- src/html/error/index.php
+++ src/html/error/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once '../../class/Config.php';
-$nav_title = 'エラーが発生しました';
+require_once '../../class/Base.php';
+
+$ins = new Base();
 
 ?>
 
@@ -15,7 +17,7 @@ $nav_title = 'エラーが発生しました';
     <link rel="stylesheet" href="../css/bootstrap5.1.3/dist/css/bootstrap.min.css">
     <!-- 自作cssの読み込み -->
     <link rel="stylesheet" href="../../css/custom.css">
-    <title><?= $nav_title ?></title>
+    <title><?= $ins->nav_title ?></title>
 </head>
 
 <body class="bg-light">
@@ -23,7 +25,7 @@ $nav_title = 'エラーが発生しました';
         <nav class="navbar fixed-top zindex-fixed p-0 opacity-75 navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand row" href="#">
-                    <h1><?= $site_title ?> |</h1>
+                    <h1><?= Config::$site_title ?> |</h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -31,7 +33,7 @@ $nav_title = 'エラーが発生しました';
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item navbar-brand">
-                            <h4><?= $nav_title ?></h4>
+                            <h4><?= $ins->nav_title ?></h4>
                         </li>
                     </ul>
                 </div>
@@ -44,7 +46,7 @@ $nav_title = 'エラーが発生しました';
             <div class="row">
                 <div class="col-1 col-md-2 col-xl-3"></div>
                 <div class="mb-4 col-10 col-md-8 col-xl-6">
-                    <p class="mb-4"><?= $err_msg ?></p>
+                    <p class="mb-4"><?= Config::$err_msg ?></p>
                     <a href="../user/logout/action.php"><button type="button" class="btn btn-danger">ログアウト</button></a>
                 </div>
                 <div class="col-1 col-md-2 col-xl-3"></div>
