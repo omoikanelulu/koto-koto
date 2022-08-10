39a7c0b2fd3595ab4ce0798bdc9c120de14b5698
omoikanelulu <r.omoikane@gmail.com>
2022-08-10 13:49:11 +0900

    インスタンスを使用するように修正

--- src/html/user/add/confirm.php
+++ src/html/user/add/confirm.php
@@ -1,6 +1,8 @@
 <?php
 require_once '../../../class/Config.php';
-$nav_title = '新規登録内容確認';
+require_once '../../../class/Base.php';
+
+$ins = new Base();
 
 ?>
 
@@ -15,15 +17,15 @@ $nav_title = '新規登録内容確認';
     <link rel="stylesheet" href="../css/bootstrap5.1.3/dist/css/bootstrap.min.css">
     <!-- 自作cssの読み込み -->
     <link rel="stylesheet" href="../../../css/custom.css">
-    <title><?= $nav_title ?></title>
+    <title><?= $ins->nav_title ?></title>
 </head>
 
 <body class="bg-light">
-    <header>
+<header>
         <nav class="navbar fixed-top zindex-fixed p-0 opacity-75 navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand row" href="<?= $top_page_url ?>">
-                    <h1><?= $site_title ?> |</h1>
+            <div class="container-fluid d-flex align-items-center">
+                <a class="navbar-brand row" href="<?= $ins->top_page_url ?>">
+                    <h1><?= Config::$site_title ?> |</h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -31,7 +33,7 @@ $nav_title = '新規登録内容確認';
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item navbar-brand">
-                            <h4><?= $nav_title ?></h4>
+                            <h4><?= $ins->nav_title ?></h4>
                         </li>
                     </ul>
                 </div>
@@ -52,7 +54,7 @@ $nav_title = '新規登録内容確認';
                     <div class="row row-cols-3 d-flex justify-content-center">
                         <div class="col">
                             <label for="user_name" class="form-label">ユーザ名</label>
-                            <input type="text" class="form-control" id="user_name" value=<?= $test_msg ?>>
+                            <input type="text" class="form-control" id="user_name" value=<?= $ins->test_msg ?>>
                         </div>
                         <div class="col"></div>
                     </div>
@@ -65,11 +67,11 @@ $nav_title = '新規登録内容確認';
                     <div class="row row-cols-3 d-flex justify-content-center">
                         <div class="col">
                             <label for="family_name" class="form-label">姓</label>
-                            <input type="text" class="form-control" id="family_name" value=<?= $test_msg ?>>
+                            <input type="text" class="form-control" id="family_name" value=<?= $ins->test_msg ?>>
                         </div>
                         <div class="col">
                             <label for="first_name" class="form-label">名</label>
-                            <input type="text" class="form-control" id="first_name" value=<?= $test_msg ?>>
+                            <input type="text" class="form-control" id="first_name" value=<?= $ins->test_msg ?>>
                         </div>
                     </div>
                     <div class="mb-4 row row-cols-3 d-flex justify-content-center">
@@ -85,7 +87,7 @@ $nav_title = '新規登録内容確認';
                             <label for="user_mail_address" class="form-label">生年月日</label>
                             <div class="input-group mb-3">
                                 <select class="form-select" id="birth_date_year">
-                                    <option selected><?= $test_msg ?></option>
+                                    <option selected><?= $ins->test_msg ?></option>
                                     <option value="2022">2022</option>
                                     <option value="2021">2021</option>
                                     <option value="2020">2020</option>
@@ -97,7 +99,7 @@ $nav_title = '新規登録内容確認';
                             <div class="col">
                                 <div class="input-group mb-3">
                                     <select class="form-select" id="birth_date_month">
-                                        <option selected><?= $test_msg ?></option>
+                                        <option selected><?= $ins->test_msg ?></option>
                                         <option value="01">01</option>
                                         <option value="02">02</option>
                                         <option value="03">03</option>
@@ -108,7 +110,7 @@ $nav_title = '新規登録内容確認';
                             <div class="col">
                                 <div class="input-group mb-3">
                                     <select class="form-select" id="birth_date_day">
-                                        <option selected><?= $test_msg ?></option>
+                                        <option selected><?= $ins->test_msg ?></option>
                                         <option value="01">01</option>
                                         <option value="02">02</option>
                                         <option value="03">03</option>
@@ -121,7 +123,7 @@ $nav_title = '新規登録内容確認';
                     <div class="row row-cols-3 d-flex justify-content-center">
                         <div class="col">
                             <label for="user_mail_address" class="form-label">メールアドレス</label>
-                            <input type="email" class="form-control" id="user_mail_address" value=<?= $test_msg ?>>
+                            <input type="email" class="form-control" id="user_mail_address" value=<?= $ins->test_msg ?>>
                         </div>
                         <div class="col"></div>
                     </div>
@@ -134,7 +136,7 @@ $nav_title = '新規登録内容確認';
                     <div class="row row-cols-3 d-flex justify-content-center">
                         <div class="col">
                             <label for="pass" class="form-label">パスワード</label>
-                            <input type="password" class="form-control" id="pass" value=<?= $test_msg ?>>
+                            <input type="password" class="form-control" id="pass" value=<?= $ins->test_msg ?>>
                         </div>
                         <div class="col"></div>
                     </div>
@@ -151,7 +153,7 @@ $nav_title = '新規登録内容確認';
                         <a href="./index.php"><button type="button" class="btn btn-secondary">前のページに戻る</button></a>
                     </div>
                     <div class="col">
-                        <a href=<?= $top_page_url ?>><button type="button" class="btn btn-danger">キャンセル</button></a>
+                        <a href=<?= $ins->top_page_url ?>><button type="button" class="btn btn-danger">キャンセル</button></a>
                     </div>
                 </div>
             </form>
