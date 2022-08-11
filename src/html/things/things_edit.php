efcbbe9a9230ca1474577dea3a87048ced8d1969
omoikanelulu <r.omoikane@gmail.com>
2022-08-11 11:06:08 +0900

    インスタンスの使用に合わせて修正

--- src/html/things/things_edit.php
+++ src/html/things/things_edit.php
@@ -1,16 +1,8 @@
 <?php
-require_once '../../class/Date.php';
-require_once '../../class/Util.php';
-require_once '../../class/Config.php'; //相対パス
-$current_page = get_uri();
+require_once '../../class/Config.php';
+require_once '../../class/Base.php';
 
-// ページに合わせてメニューの表示を変える処理
-foreach ($nav_menus as $menu => $url) {
-    if ($current_page == $url) {
-        $nav_title = $menu;
-        break;
-    }
-}
+$ins = new Base();
 
 ?>
 
@@ -25,32 +17,29 @@ foreach ($nav_menus as $menu => $url) {
     <link rel="stylesheet" href="../../css/bootstrap5.1.3/dist/css/bootstrap.min.css">
     <!-- 自作cssの読み込み -->
     <link rel="stylesheet" href="../../css/custom.css">
-    <title><?= $nav_title ?></title>
+    <title><?= $ins->nav_title ?></title>
 </head>
 
 <body class="bg-light">
     <header>
         <nav class="navbar fixed-top zindex-fixed p-0 opacity-75 navbar-expand-lg navbar-dark bg-dark">
             <div class="navbar-text container-fluid row-cols">
-                <a class="navbar-brand row" href="<?= $top_page_url ?>">
-                    <h1><?= $site_title ?> |</h1>
+                <a class="navbar-brand row" href="<?= $ins->top_page_url ?>">
+                    <h1><?= Config::$site_title ?> |</h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-lg-0">
-                        <!-- <li class="nav-item navbar-brand">
-                            <h4><//?= $nav_title ?></h4>
-                        </li> -->
                         <!-- ここからドロップダウンメニュー -->
                         <!-- ページ移動メニュー -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <?= $nav_title ?>
+                                <?= $ins->nav_title ?>
                             </a>
-                            <ul class="text-end dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
-                                <?php foreach ($nav_menus as $menu => $url) : ?>
+                            <ul class="text-start dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <?php foreach ($ins->nav_menus['links'] as $menu => $url) : ?>
                                     <li><a class="dropdown-item" href="<?= $url ?>"><?= $menu ?></a></li>
                                 <?php endforeach ?>
                             </ul>
@@ -89,8 +78,8 @@ foreach ($nav_menus as $menu => $url) {
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 user_name
                             </a>
-                            <ul class="text-end dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
-                                <?php foreach ($nav_user_menus as $menu => $url) : ?>
+                            <ul class="text-start dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <?php foreach ($ins->nav_user_menus as $menu => $url) : ?>
                                     <li><a class="dropdown-item" href=<?= $url ?>><?= $menu ?></a></li>
                                 <?php endforeach ?>
                             </ul>
@@ -100,7 +89,6 @@ foreach ($nav_menus as $menu => $url) {
                 </div>
             </div>
         </nav>
-
     </header>
 
     <main>
