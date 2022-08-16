b0f66535d07f4d8e18ebb23afc86409f49fd6807
lulu <r.omoikane@gmail.com>
2022-08-16 12:00:38 +0900

    require_once追加

--- src/html/user/add/action.php
+++ src/html/user/add/action.php
@@ -3,6 +3,8 @@ require_once '../../../class/Config.php';
 require_once '../../../class/Base.php';
 require_once '../../../class/Security.php';
 require_once '../../../class/Validation.php';
+require_once '../../../class/DB_Base.php';
+require_once '../../../class/DB_Users.php';
 
 // セッションスタート
 Security::session();
