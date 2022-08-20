600cdb9d551e6378b58b6b5783e1a791de821e81
lulu <r.omoikane@gmail.com>
2022-08-20 17:26:18 +0900

    ログアウト処理を追加

--- src/html/user/logout/action.php
+++ src/html/user/logout/action.php
@@ -1,5 +1,17 @@
 <?php
-echo 'ログアウト処理を行う、';
-echo 'その後トップページへ遷移';
+require_once '../../../class/Config.php';
+require_once '../../../class/Base.php';
+require_once '../../../class/Security.php';
+require_once '../../../class/Validation.php';
+
+// セッションスタート
+Security::session();
+
+// インスタンス作成
+$ins = new Base();
+
+unset($_SESSION['login_user']);
+
+header('Location:'.$ins->top_page_url);
 
 ?>
