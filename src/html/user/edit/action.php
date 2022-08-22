c7b88ab53f78bb3611ed459dd5c61c91641ce219
lulu <r.omoikane@gmail.com>
2022-08-22 21:51:24 +0900

    ザックリ記述

--- src/html/user/edit/action.php
+++ src/html/user/edit/action.php
@@ -1,2 +1,29 @@
 <?php
-echo '編集内容でDBをupdateする、その後success画面へ遷移';
+try {
+    require_once '../../../class/Config.php';
+    require_once '../../../class/Base.php';
+    require_once '../../../class/Security.php';
+    require_once '../../../class/Validation.php';
+    require_once '../../../class/DB_Base.php';
+    require_once '../../../class/DB_Users.php';
+
+    // セッションスタート
+    Security::session();
+    $post = $_SESSION['input_user_data'];
+
+    $ins = new Base;
+
+    // インスタンス生成、新規ユーザ登録する
+    $DBins = new DB_Users;
+    // trueならsuccessページへ遷移する
+    $result = $DBins->userUpdate($post);
+    if ($result == true) {
+        unset($_SESSION['input_user_data'],$_SESSION['exception']);
+        header('Location:./success.php');
+        exit();
+    }
+} catch (Exception $e) {
+    $_SESSION['exception'] = $e;
+    header('Location:' . $ins->err_page_url);
+    exit();
+}
