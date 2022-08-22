64c51d1607e3881210b2f56ad81c1aa7b97ef847
omoikanelulu <r.omoikane@gmail.com>
2022-08-22 14:46:22 +0900

    require_onceの階層とform-textの表示を修正

--- src/html/user/edit/index.php
+++ src/html/user/edit/index.php
@@ -1,10 +1,10 @@
 <?php
-require_once '../../class/Config.php';
-require_once '../../class/Base.php';
-require_once '../../class/Security.php';
-require_once '../../class/Validation.php';
-require_once '../../class/DB_Base.php';
-require_once '../../class/DB_Users.php';
+require_once '../../../class/Config.php';
+require_once '../../../class/Base.php';
+require_once '../../../class/Security.php';
+require_once '../../../class/Validation.php';
+require_once '../../../class/DB_Base.php';
+require_once '../../../class/DB_Users.php';
 
 Security::session();
 
@@ -23,7 +23,7 @@ $ins = new Base();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap cssの読み込み -->
-    <link rel="stylesheet" href="../css/bootstrap5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../css/bootstrap5.1.3/dist/css/bootstrap.min.css">
     <!-- 自作cssの読み込み -->
     <link rel="stylesheet" href="../../../css/custom.css">
     <title><?= $ins->nav_title ?></title>
@@ -57,25 +57,25 @@ $ins = new Base();
                     <div class="mb-4 row row-cols-2 d-flex justify-content-center">
                         <div class="col">
                             <label for="user_mail_address" class="form-label">メールアドレス</label>
-                            <input type="email" class="form-control" id="user_mail_address" placeholder="hoge@example.com">
+                            <input type="email" class="form-control" name="user_mail_address" id="user_mail_address" placeholder="hoge@example.com">
                         </div>
                     </div>
                     <div class="mb-4 row row-cols-2 d-flex justify-content-center">
                         <div class="col">
                             <label for="pass" class="form-label">パスワード</label>
-                            <input type="password" class="form-control" id="pass" placeholder="your_password">
+                            <input type="password" class="form-control" name="pass" id="pass" placeholder="your_password">
                         </div>
                     </div>
                 </fieldset>
                 <div class="row row-cols-2 d-flex justify-content-center">
                     <div class="col form-text text-danger">
-                        NG message
+                        <?= isset($_SESSION['err']['err_checkId']) ? $_SESSION['err']['err_checkId'] : '' ?>
                     </div>
                 </div>
                 <div class="row row-cols-2 d-flex justify-content-center">
                     <div class="col">
                         <input type="submit" class="me-3 btn btn-primary" value="ログイン">
-                        <a href="<?= $ins->top_page_url ?>"><input type="button" class="btn btn-danger" value="キャンセル"></a>
+                        <a href="<?= $ins->things_top_page_url ?>"><input type="button" class="btn btn-danger" value="キャンセル"></a>
                     </div>
                 </div>
             </form>
@@ -83,10 +83,17 @@ $ins = new Base();
     </main>
 
     <footer>
+        <?php
+        // デバッグ用 //
+        echo '<pre>';
+        var_dump($_SESSION);
+        echo '</pre>';
+        ////////////////
+        ?>
     </footer>
 
     <!-- bootstrap JavaScript Bundle with Popper -->
-    <script src="../css/bootstrap5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../../css/bootstrap5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
