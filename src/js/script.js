faa6172fa2e712209b2be43848a7efbd6fde97de
omoikanelulu <r.omoikane@gmail.com>
2022-08-23 15:38:33 +0900

    チェックを外すとformの値が削除される処理を追加

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,33 @@
 function userNameDisabled(user_name, is_checked) {
     if (is_checked == true) {
-        // チェックが入っていたら有効化
+        // チェックが入っていたらform有効化
         document.getElementById(user_name).disabled = false;
     }
     else {
-        // チェックが入っていなかったら無効化
+        // チェックが入っていなかったらform無効化
         document.getElementById(user_name).disabled = true;
+        document.getElementById(user_name).value = '';
     }
 }
 
 function userMailAddressDisabled(user_mail_address, is_checked) {
     if (is_checked == true) {
-        // チェックが入っていたら有効化
+        // チェックが入っていたらform有効化
         document.getElementById(user_mail_address).disabled = false;
     }
     else {
-        // チェックが入っていなかったら無効化
+        // チェックが入っていなかったらform無効化
         document.getElementById(user_mail_address).disabled = true;
     }
 }
 
 function passDisabled(pass, is_checked) {
     if (is_checked == true) {
-        // チェックが入っていたら有効化
+        // チェックが入っていたらform有効化
         document.getElementById(pass).disabled = false;
     }
     else {
-        // チェックが入っていなかったら無効化
+        // チェックが入っていなかったらform無効化
         document.getElementById(pass).disabled = true;
     }
 }
