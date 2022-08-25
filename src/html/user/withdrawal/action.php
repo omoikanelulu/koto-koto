2e0141cc9a9aafd504ae6596e3a63dfa16e9e094
omoikanelulu <r.omoikane@gmail.com>
2022-08-25 14:01:33 +0900

    処理を作成

--- src/html/user/withdrawal/action.php
+++ src/html/user/withdrawal/action.php
@@ -1,3 +1,32 @@
 <?php
-echo 'ユーザ退会処理を行う、その後退会処理完了画面へ遷移';
-?>
+try {
+    require_once '../../../class/Config.php';
+    require_once '../../../class/Base.php';
+    require_once '../../../class/Security.php';
+    require_once '../../../class/Validation.php';
+    require_once '../../../class/DB_Base.php';
+    require_once '../../../class/DB_Users.php';
+
+    // セッションスタート
+    Security::session();
+    $login_user = $_SESSION['login_user'];
+
+    $ins = new Base;
+    $DBins = new DB_Users;
+
+    // userUpdateを呼び出す
+    $result = $DBins->deletedFlagOn($login_user);
+    // trueならsuccessページへ遷移する
+    if ($result == true) {
+        unset($_SESSION['edit_user_data'], $_SESSION['exception'], $_SESSION['err']);
+        header('Location:./success.php');
+        exit();
+    } else {
+        throw new Exception('UPDATEに失敗しました');
+        exit();
+    }
+} catch (Exception $e) {
+    $_SESSION['exception'] = $e;
+    header('Location:' . $ins->err_page_url);
+    exit();
+}
