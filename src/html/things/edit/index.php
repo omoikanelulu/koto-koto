c42477054557d9821c36b5bd8070e7c813015612
lulu <r.omoikane@gmail.com>
2022-08-28 17:50:30 +0900

    addからまるっとコピーしたところ、修正が必要

--- src/html/things/edit/index.php
+++ src/html/things/edit/index.php
@@ -13,6 +13,13 @@ Security::notLogin();
 
 $ins = new Base();
 
+// $things = データベースから引っ張ってきたthings内容
+
+// 現在の日付を取得 $date->format('Y/n/d'); // 2016/1/25
+$date = $_SESSION['things']['create_date_time'];
+// フォーマットを整えて変数に代入
+$create_date_time = $date->format('Y/n/d');
+
 ?>
 
 <!DOCTYPE html>
@@ -85,7 +92,7 @@ $ins = new Base();
                     <ul class="navbar-nav mb-lg-0 d-flex justify-content-end">
                         <li class="nav-item dropstart">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <?= isset($_SESSION['login_user']['user_name']) ? $_SESSION['login_user']['user_name'] : '' ?>
+                                <?= isset($_SESSION['login_user']['user_name']) ? $_SESSION['login_user']['user_name'] : '' ?>
                             </a>
                             <ul class="text-start dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
                                 <?php foreach ($ins->nav_user_menus as $menu => $url) : ?>
@@ -102,46 +109,81 @@ $ins = new Base();
 
     <main>
         <div class="mt-5 container">
-            <div class="row row-cols-2 d-flex justify-content-center">
-                <div class="col">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <h5>使い方</h5>
-                        </div>
-                        <div class="card-body">
-                            <ol>
-                                <li class="card-text">深く考えずにメモ感覚でデキゴトを記録する</li>
-                                <li class="card-text">記録したデキゴトをふり返る</li>
-                                <li class="card-text">そのデキゴトがイイコトだったのかヤナコトだったのか仕分ける</li>
-                                <li class="card-text">今日のイイコトベスト3を決める（就寝前に行うのが良い）</li>
-                                <li class="card-text">ヤナコトに対する対処法を考える（ストレスに対する対処法を持つ事でストレス軽減に繋がる）</li>
-                            </ol>
-                        </div>
-                    </div>
+            <div class="row justify-content:flex-start">
+                <div class="col-sm">
+                    <h1 class="right_bg_line"><?= $create_date_time ?></h1>
                 </div>
             </div>
-            <div class="row row-cols-2 d-flex justify-content-center">
-                <div class="col">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <h5>なぜなに「koto-koto」</h5>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="mb-2 card-text">何をするサイトなの？</h5>
-                            <p class="card-text">当サイトは日々のデキゴトを記録し、イイコト（良かった事、楽しかった事、嬉しかった事など）を振り返ったり、
-                                ヤナコト（悪かった事、悲しかった事、辛かった事など）に対してどの様に対処するのか考える事で、</p>
-                            <ul>
-                                <li class="card-text">自己肯定感の向上</li>
-                                <li class="card-text">ストレスに対する対処の仕方を考え日々のストレスを軽減する</li>
-                            </ul>
-                            <p class="card-text">といった、セルフケアのお手伝いが出来れば良いな、という趣旨で制作しております。</p>
+
+            <!-- デキゴト入力ブロック -->
+            <form action="" method="post">
+                <div class="row mt-4 justify-content-end">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-auto">
+                        <label class="form-label" for="thing">デキゴトの編集 <!-- rowとcolはCSSで設定する方が良い？ -->
+                            <textarea class="form-control" name="thing" id="thing" cols="80" rows="5" maxlength="200" aria-valuetext=""><?= $thing // ここがvalueの値になるらしい ?></textarea>
+                            <div class="form-text"><?= Config::TIPS_LL_THING ?></div>
+                        </label>
+                    </div>
+                    <div class="col-sm"></div>
+                </div>
+
+                <!-- 属性付与ブロック -->
+                <div class="row mt-4 justify-content-start">
+                    <div class="col-sm-2"></div>
+                    <!-- イイコトブロック -->
+                    <div class="col-sm-auto align-self-center">
+                        <input class="form-check-input" type="checkbox" name="cb_good_thing" id="cb_good_thing" value="1">
+                        <label class="form-check-label" for="cb_good_thing">イイコト</label>
+                    </div>
+                    <div class="col-sm-auto align-self-center">
+                        <div class="input-group">
+                            <label class="input-group-text" for="good_thing_level">イイコトレベル</label>
+                            <select class="form-select" name="good_thing_level" id="good_thing_level">
+                                <?php foreach (Config::GOOD_THING_LEVEL as $i) : ?>
+                                    <option value="$i"><?= $i ?></option>
+                                <?php endforeach ?>
+                            </select>
                         </div>
                     </div>
+
+                    <!-- ヤナコトブロック -->
+                    <div class="col-sm-auto align-self-center">
+                        <input class="form-check-input" type="checkbox" name="cb_bad_thing" id="cb_bad_thing" value="1">
+                        <label class="form-check-label" for="cb_bad_thing">ヤナコト</label>
+                    </div>
+                    <div class="col-sm-auto align-self-center">
+                        <div class="input-group">
+                            <label class="input-group-text" for="bad_thing_level">ヤナコトレベル</label>
+                            <select class="form-select" name="bad_thing_level" id="bad_thing_level">
+                                <?php foreach (Config::BAD_THING_LEVEL as $i) : ?>
+                                    <option value="$i"><?= $i ?></option>
+                                <?php endforeach ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-sm"></div>
                 </div>
-            </div>
+                <div class="row mt-4 justify-content-start">
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-auto">
+                        <button class="me-3 btn btn-primary" type="submit">登録する</button>
+                        <button class="me-3 btn btn-secondary" type="reset">書き直す</button>
+                        <button class="me-3 btn btn-danger" type="reset">キャンセル</button>
+                    </div>
+                    <div class="col-sm"></div>
+                </div>
+            </form>
         </div>
     </main>
     <footer>
+        <?php
+        // デバッグ用 //
+        echo '<pre>';
+        var_dump($_SESSION);
+        echo '</pre>';
+        ////////////////
+        ?>
     </footer>
 
     <!-- bootstrap JavaScript Bundle with Popper -->
