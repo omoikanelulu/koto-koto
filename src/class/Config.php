2a3c3dc570881f37920517bc3e629983e195436c
omoikanelulu <r.omoikane@gmail.com>
2022-09-01 15:22:56 +0900

    弱中強を小中大へ変更

--- src/class/Config.php
+++ src/class/Config.php
@@ -13,7 +13,7 @@ class Config //staticプロパティは内容が変化しないのものを設�
 
     // things関係
     public const GOOD_THING_RANK = array('1', '2', '3');
-    public const BAD_THING_LEVEL = array('弱' => '1', '中' => '2', '強' => '3');
+    public const BAD_THING_LEVEL = array('小' => '1', '中' => '2', '大' => '3');
 
     // 文字数制限（user関係）
     public const LL_USER_NAME = '50';
