8d13f580a94207e2e26c4ad2de39e3736f60c4bd
omoikanelulu <r.omoikane@gmail.com>
2022-09-02 11:26:02 +0900

    対処法のバリデーションを追加、エラーがあった時に戻る処理を追加

--- src/html/things/add/action.php
+++ src/html/things/add/action.php
@@ -11,14 +11,34 @@ Security::session();
 Security::notLogin();
 
 // 送信されてきたデータをサニタイズして変数に代入
-$input_thing = Security::sanitize($_POST);
+$post = Security::sanitize($_POST);
+// $_SESSIONにも代入
+$_SESSION['post_data'] = $post;
 
 // ここからバリデーション
+$has_err = '';
+$result = '';
+unset($_SESSION['err']);
+
 // 文字数チェック
-$result = Validation::llCheck($input_thing['thing'], Config::LL_THING);
+$result = Validation::llCheck($post['thing'], Config::LL_THING);
 
 if ($result == false) {
-    $_SESSION['err']['err_llCheck'] = Config::ERR_LL_THING;
+    $_SESSION['err']['err_llThing'] = Config::ERR_LL_THING;
+    $result = '';
+    $has_err = true;
+}
+
+$result = Validation::llCheck($post['bad_thing_approach'], Config::LL_APPROACH);
+
+if ($result == false) {
+    $_SESSION['err']['err_llApproach'] = Config::ERR_LL_APPROACH;
+    $result = '';
+    $has_err = true;
+}
+
+// エラーがあったらページをリダイレクトで戻す
+if ($has_err == true) {
     header('Location:./index.php', true, 307);
     exit();
 }
@@ -28,8 +48,9 @@ if ($result == false) {
 $ins = new Base;
 $DBins = new DB_Things;
 
+// データベースに登録
 try {
-    $result = $DBins->thingsAdd($input_thing, $_SESSION['login_user']['id']);
+    $result = $DBins->thingsAdd($post, $_SESSION['login_user']['id']);
     if ($result == true) {
         unset($_SESSION['input_thing'], $_SESSION['err']['err_llCheck'], $_SESSION['exception']);
         header('Location:./success.php');
