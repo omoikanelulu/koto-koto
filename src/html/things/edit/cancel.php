bd4b9e13c9a574a5d136ffc9bde206f1ae6180b4
omoikanelulu <r.omoikane@gmail.com>
2022-09-21 10:02:00 +0900

    新規作成

---
+++ src/html/things/edit/cancel.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../../../class/Base.php';
+require_once '../../../class/Security.php';
+
+Security::session();
+
+// ログインしていない場合トップページへリダイレクトする
+Security::notLogin();
+
+// $_SESSIONにユーザが入力したデータがあればunsetする
+if (isset($_SESSION['edit_thing']) == true) {
+    unset($_SESSION['edit_thing']);
+}
+// $_SESSIONにエラーメッセージがあればunsetする
+if (isset($_SESSION['err']) == true) {
+    unset($_SESSION['err']);
+}
+// $_SESSIONにverifiedがあればunsetする
+if (isset($_SESSION['verified']['confirm']) == true) {
+    unset($_SESSION['verified']['confirm']);
+}
+if (isset($_SESSION['verified']['checkId']) == true) {
+    unset($_SESSION['verified']['checkId']);
+}
+if (isset($_SESSION['verified']['action']) == true) {
+    unset($_SESSION['verified']['action']);
+}
+
+$ins = new Base;
+
+header('Location:' . $ins->thing_show_page_url);
+exit();
