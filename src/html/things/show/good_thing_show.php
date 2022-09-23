e80b3d47ee08b1f23a8d3ca9d2b4ed6c5374f45e
omoikanelulu <r.omoikane@gmail.com>
2022-09-23 14:51:26 +0900

    navbarとbodyのレイアウトをthing_showと揃えた

--- src/html/things/show/good_thing_show.php
+++ src/html/things/show/good_thing_show.php
@@ -47,20 +47,23 @@ $things = $DBins->goodThingShow($_SESSION['login_user']['id'], $search_date);
 
 <body class="bg-light">
 <header>
-        <nav class="navbar bg-opacity-75 fixed-top zindex-fixed p-0 navbar-expand-md navbar-dark bg-dark">
-            <div class="navbar-text container-fluid align-item-center">
-                <a class="navbar-brand" href="<?= $ins->top_page_url ?>">
+        <nav class="navbar bg-opacity-75 fixed-top zindex-fixed justify-content-center p-0 navbar-expand-md navbar-dark bg-dark">
+            <div class="navbar-text container-fluid">
+                <a class="navbar-brand d-flex align-items-center" href="<?= $ins->top_page_url ?>">
                     <h1><?= Config::SITE_TITLE ?> |</h1>
                 </a>
+                <!-- 幅が小さくなると表示されるボタンを用意する、この中にメニューが入る -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
+                <!-- この中身idで括っている部分がボタンの中に入る事になる -->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-1 mb-lg-0">
-                        <!-- ここからドロップダウンメニュー -->
-                        <!-- ページ移動メニュー -->
+
+                    <!-- ページ移動メニュー -->
+                    <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="navbar-text nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <?= $ins->nav_title ?>
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
@@ -71,23 +74,10 @@ $things = $DBins->goodThingShow($_SESSION['login_user']['id'], $search_date);
                         </li>
                     </ul>
 
-                    <!-- 年月日の入力フォーム -->
-                    <form class="row me-auto d-flex justify-content-start" id="search_date_form" action="#" method="post">
-                        <div class="navbar-nav mb-lg-0">
-                            <div class="col-sm input-group">
-                                <input type="date" name="search_date" id="search_date_input" pattern=”[0-9]{4}-[0-9]{2}-[0-9]{2}” value=<?= $search_date ?>>
-                                <i class="bi bi-calendar" id="search_date_icon"></i>
-                            </div>
-                            <div class="col-sm input-group">
-                                <input class="btn" type="submit" id="search_date_submit" value="以降を表示">
-                            </div>
-                        </div>
-                    </form>
-
                     <!-- ユーザメニュー -->
-                    <ul class="navbar-nav mb-lg-0 d-flex justify-content-end">
-                        <li class="nav-item dropstart">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="navbar-text nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <?= isset($_SESSION['login_user']['user_name']) ? $_SESSION['login_user']['user_name'] : '' ?>
                             </a>
                             <ul class="text-start dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
@@ -97,8 +87,21 @@ $things = $DBins->goodThingShow($_SESSION['login_user']['id'], $search_date);
                             </ul>
                         </li>
                     </ul>
-                    <!-- ここまでドロップダウンメニュー -->
+
+                    <!-- 年月日の入力フォーム -->
+                    <div class="navbar-nav col-md-auto">
+                        <form class="d-flex justify-content-start" id="search_date_form" action="#" method="post">
+                            <div class="row mb-lg-0">
+                                <div class="px-0 col-sm input-group">
+                                    <input class="me-2" type="date" name="search_date" id="search_date_input" pattern=”[0-9]{4}-[0-9]{2}-[0-9]{2}” value=<?= $search_date ?>>
+                                    <i class="bi bi-calendar" id="search_date_icon"></i>
+                                    <input class="btn rounded-pill" type="submit" id="search_date_submit" value="以降を表示">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+                <!-- ここまで この中身idで括っている部分がボタンの中に入る事になる -->
             </div>
         </nav>
     </header>
@@ -117,27 +120,35 @@ $things = $DBins->goodThingShow($_SESSION['login_user']['id'], $search_date);
                             <!-- mb_substr(文字列,取り出したい文字の開始位置,開始位置から取り出す文字の数) -->
                             <h2 class="right_bg_line"><?= mb_substr(str_replace('-', '/', $thing['create_date_time']), 0, 16) ?></h2>
 
-                            <!-- イイコトの順位表示 -->
-                            <div class="row m-2 mb-4 justify-content-start align-items-center">
-                                <div class="col-sm-1 text-center">
+                            <div class="row m-2 justify-content-start align-items-center">
+                                <!-- イイコトの順位表示 -->
+                                <div class="col-sm-2 col-md-1 text-center">
                                     <p class="mb-0 bg-good-thing rounded-pill"><?= $thing['good_thing_flag'] == '0' ? '' : $thing['good_thing_rank'] ?></p>
                                 </div>
 
                                 <!-- ヤナコトの強度表示 -->
-                                <div class="col-sm-1 text-center">
+                                <div class="col-sm-2 col-md-1 text-center">
                                     <p class="mb-0 bg-bad-thing rounded-pill"><?= $thing['bad_thing_flag'] == '0' ? '' : $thing['bad_thing_level'] ?></p>
                                 </div>
 
                                 <!-- thingを表示 -->
-                                <div class="col-sm-8">
+                                <div class="col-sm-8 col-md-10 justify-content-start">
                                     <p class="mb-0"><?= $thing['thing'] ?></p>
                                 </div>
 
+                            </div>
+                            <div class="row m-2">
+                                <!-- 対処法が保存されていたら表示 -->
+                                <div class="col-sm-10 offset-sm-2 d-flex justify-content-end align-items-center">
+                                    <p class="<?= empty($thing['bad_thing_approach']) ? 'invisible' : '' ?> bg-bad-approach rounded-pill"><?= $thing['bad_thing_approach'] ?></p>
+                                </div>
+                            </div>
+                            <div class="row justify-content-end m-2">
                                 <!-- 各種ボタンを表示 -->
-                                <div class="col-sm-1 text-center">
+                                <div class="col-auto text-center">
                                     <a href="../edit/index.php?id=<?= urlencode($thing['id']) ?>"><i class="bi bi-pencil">編集</i></a>
                                 </div>
-                                <div class="col-sm-1 text-center">
+                                <div class="col-auto text-center">
                                     <a href="../delete/action.php?id=<?= urlencode($thing['id']) ?>"><i class="bi bi-trash">削除</i></a>
                                 </div>
                             </div>
